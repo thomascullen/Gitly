2fc01b1e69e6d20bc4c5b62625096c0158fbcb9d
Brian Kenny <brian@bkenny.com>
2014-01-29 14:50:24 +0000

    Confirm email address ready to go

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "tim@gitly.co"
+  default from: "Gitly <no-reply@gitly.co>"
 
   def validate_user(user)
   	@user = user
 
-  	mail(to: @user.email, subject: "Validate your email address")
+  	mail(to: @user.email, subject: "Confirm your email address")
   end
 end
